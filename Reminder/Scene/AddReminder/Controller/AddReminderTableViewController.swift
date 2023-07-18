986f06d12e37a5a4ae7a88d15cc0888d66886c63
baris dilekci <bdilekci200@gmail.com>
2023-07-19 00:25:58 +0300

    feat: datepicker and tableview codes were written in addReminderController

--- Reminder/Scene/AddReminder/Controller/AddReminderTableViewController.swift
+++ Reminder/Scene/AddReminder/Controller/AddReminderTableViewController.swift
@@ -8,49 +8,115 @@
 import UIKit
 
 class AddReminderTableViewController: UITableViewController {
-
+    
+    //MARK: - UI Elements
     @IBOutlet weak var reminderLabel: UILabel!
     @IBOutlet weak var reminderDatePicker: UIDatePicker!
     @IBOutlet weak var reminderSwitch: UISwitch!
     @IBOutlet weak var reminderTextField: UITextField!
     
+    //MARK: - Properties
+    var reminder: Reminder?
+    let reminderManger = ReminderManager()
+    
+    let dateLabelCellIndexPath = IndexPath(row: 1, section: 1)
+    let datePickerCellIndexPath = IndexPath(row: 2, section: 1)
+    
+    var isDatePickerShown = false
+    
+    
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        datePickerSettings()
+        updateDateView()
+        
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        reminderTextField.becomeFirstResponder()
+    }
+    
+    
+    
+    
+    //MARK: - Actions
     @IBAction func saveButtonClicked(_ sender: UIBarButtonItem) {
     }
     @IBAction func cancelButtonClicked(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
     }
     @IBAction func datePickerChanged(_ sender: UIDatePicker) {
+        updateDateView()
     }
     @IBAction func reminderSwitchChanged(_ sender: UISwitch) {
+        if !sender.isOn {
+                isDatePickerShown = false
+            }
+            
+            updateCells()
     }
 }
+
+
+//MARK: - Helpers
+extension AddReminderTableViewController {
+    private func updateDateView() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .short
+        dateFormatter.locale = Locale(identifier: "tr")
+        
+        reminderLabel.text = dateFormatter.string(from: reminderDatePicker.date)
+    }
+    
+    private func datePickerSettings() {
+        var dateComponents = DateComponents()
+        dateComponents.setValue(2, for: .minute)
+        
+        reminderDatePicker.minimumDate = Calendar.current.date(byAdding: dateComponents, to: Date())!
+    }
+    
+    func updateCells() {
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
+}
+
+
 
 // MARK: - Table view data source
 
 extension AddReminderTableViewController {
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
+        
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case dateLabelCellIndexPath:
+            if reminderSwitch.isOn {
+                return 44
+            } else {
+                return 0
+            }
+            
+        case datePickerCellIndexPath:
+            if isDatePickerShown && reminderSwitch.isOn {
+                return 216
+            } else {
+                return 0
+            }
+        default:
+            return 44
+        }
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 2
-    }
-
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if indexPath == dateLabelCellIndexPath {
+            isDatePickerShown.toggle()
+            updateCells()
+        }
     }
-    
-
 }
